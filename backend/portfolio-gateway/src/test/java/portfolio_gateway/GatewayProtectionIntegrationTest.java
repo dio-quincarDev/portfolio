ecb8c46795263838493b5d6eb4d4498d15210ae1
dio-quincar <dio-quincar@outlook.com>
2026-05-20 11:59:21 -0500

    CI pipeline gateway issue test

--- backend/portfolio-gateway/src/test/java/portfolio_gateway/GatewayProtectionIntegrationTest.java
+++ backend/portfolio-gateway/src/test/java/portfolio_gateway/GatewayProtectionIntegrationTest.java
@@ -3,9 +3,11 @@ package portfolio_gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class GatewayProtectionIntegrationTest {
 
     @Autowired
